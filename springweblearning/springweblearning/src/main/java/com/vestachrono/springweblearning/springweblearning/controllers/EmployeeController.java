9f7491abea7cbea9d016d79787ece916b7efa204
Manoj Shiv <manojshivaprakash10@gmail.com>
2024-07-11 22:55:42 +0530

    2.3The persistance Layer and JPA repostory (Hibernate, Lombok, H2)

--- springweblearning/springweblearning/src/main/java/com/vestachrono/springweblearning/springweblearning/controllers/EmployeeController.java
+++ springweblearning/springweblearning/src/main/java/com/vestachrono/springweblearning/springweblearning/controllers/EmployeeController.java
@@ -2,14 +2,15 @@ package com.vestachrono.springweblearning.springweblearning.controllers;
 
 
 import com.vestachrono.springweblearning.springweblearning.dto.EmployeeDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.vestachrono.springweblearning.springweblearning.entities.EmployeeEntity;
+import com.vestachrono.springweblearning.springweblearning.repositories.EmployeeRepository;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
+@RequestMapping(path = "/employees")
 public class EmployeeController {
 
 //    @GetMapping(path = "/getSecretMessage")
@@ -17,15 +18,36 @@ public class EmployeeController {
 //        return "This is my Secret message @#&&!^(&&^!^&*";
 //    }
 
-    @GetMapping(path = "/employees/{employeeId}")
-    public EmployeeDTO getEmployeeByID(@PathVariable Long employeeId){
-        return new EmployeeDTO(employeeId, "Manoj", "manoj@gmail.com", 24, LocalDate.of(2024, 1,20),true);
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
 
-    @GetMapping(path = "/employees")
-    public String getAllEmployees(@RequestParam(required = false) Integer age,
-                                  @RequestParam(required = false) String sortBy){
-        return "Hi age " + age + " " + sortBy;
+    @GetMapping(path = "/{employeeId}")
+    public EmployeeEntity getEmployeeByID(@PathVariable(name = "employeeId") Long id){
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    @GetMapping
+    public List<EmployeeEntity> getAllEmployees(@RequestParam(required = false, name = "inputAge") Integer age,
+                                                @RequestParam(required = false) String sortBy){
+        return employeeRepository.findAll();
+    }
+
+    @PostMapping
+    public EmployeeEntity createNewEmployee(@RequestBody EmployeeEntity inputEmployee) {
+        return employeeRepository.save(inputEmployee);
+    }
+
+    @PutMapping
+    public String updateEmployeeById() {
+        return  "Hello from PUT";
+    }
+
+    @PatchMapping
+    public String updateEmployee() {
+        return  "Hello from PUT";
     }
 
 }
