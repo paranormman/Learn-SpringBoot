af9aef3a43f14ba0976693d26ea8a67f86ae6f22
Manoj Shiv <manojshivaprakash10@gmail.com>
2024-07-25 00:07:00 +0530

    Week2 MVC Architecture and RESTful API's

--- springweblearning/springweblearning/src/main/java/com/vestachrono/springweblearning/springweblearning/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.vestachrono.springweblearning.springweblearning.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
