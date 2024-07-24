5521e5d4b3d705a0fac934973a53ca23a63dc183
Manoj Shiv <48378983+paranormman@users.noreply.github.com>
2024-07-25 00:36:40 +0530

    Delete Week2-Spring_MVC-RESTful_APIs directory

--- Week2-Spring_MVC-RESTful_APIs/DepartmentWeek2/src/main/java/com/Manoj/SpringBootWeek2Task/DepartmentWeek2/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.Manoj.SpringBootWeek2Task.DepartmentWeek2.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
