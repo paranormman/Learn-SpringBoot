7864ac4788261a6c4e47ab5752ab363a98d2c618
Manoj Shiv <manojshivaprakash10@gmail.com>
2024-07-26 23:04:57 +0530

    3.2 Hibernate ORM, JPA, Entities and Tables in Spring data JPA

--- Week3-Hibernate_SpringJPA/JPA_Hibernate/src/main/java/com/vestachrono/jpaHibernate/JPA_Hibernate/entity/Product.java
+++ Week3-Hibernate_SpringJPA/JPA_Hibernate/src/main/java/com/vestachrono/jpaHibernate/JPA_Hibernate/entity/Product.java
@@ -1,11 +1,10 @@
 package com.vestachrono.jpaHibernate.JPA_Hibernate.entity;
 
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -16,15 +15,34 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(
+        name = "product_table",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "title_price_unique", columnNames = {"title_x", "price"})
+        },
+        indexes = {
+                @Index(name = "sku_index", columnList = "sku")
+        }
+)
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 20)
     private String sku;
+
+    @Column(name = "title_x")
     private String title;
+
     private BigDecimal price;
+
     private Integer quantity;
+
+    @CreationTimestamp()
     private LocalDateTime createdAt;
+
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 }
