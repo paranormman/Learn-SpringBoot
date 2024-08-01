dcc6039107cdf2119340361ef5baddb3e0c0a212
Manoj Shiv <manojshivaprakash10@gmail.com>
2024-08-01 21:54:59 +0530

    Week4 Production ready features

---
+++ Week4-Production_Ready-Features/Prod_ready_features/src/test/java/com/vestaChrono/prod_ready_features/Prod_ready_features/ProdReadyFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vestaChrono.prod_ready_features.Prod_ready_features;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdReadyFeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
